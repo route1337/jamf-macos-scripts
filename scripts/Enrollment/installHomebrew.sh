382d252e8b566ae8076d2b76b84198de60a9248f
Matthew Ahrenstein <matt@ahrenstein.com>
2022-03-22 00:43:57 -0400

    XCode fixes

--- scripts/Enrollment/installHomebrew.sh
+++ scripts/Enrollment/installHomebrew.sh
@@ -12,6 +12,9 @@
 # Contributors:
 # - "Dakr-xv": https://github.com/Dakr-xv
 #
+# Using MIT Licensed Code From:
+# - "rtrouton": https://github.com/rtrouton
+#
 # See LICENSE
 #
 
@@ -26,45 +29,51 @@ else
   ConsoleUser="$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')"
 fi
 
-# Check to see if we have XCode already
-checkForXcode=$( pkgutil --pkgs | grep com.apple.pkg.CLTools_Executables | wc -l | awk '{ print $1 }' )
+### Script from rtrouton ###
+# Source: https://github.com/ryangball/rtrouton_scripts/blob/main/rtrouton_scripts/install_xcode_command_line_tools/install_xcode_command_line_tools.sh
 
-# If XCode is missing we will install the Command Line tools only as that's all Homebrew needs
-if [[ "$checkForXcode" != 1 ]]; then
-  # Save current IFS state
-  OLDIFS=$IFS
-  IFS='.' read osvers_major osvers_minor osvers_dot_version <<< "$(/usr/bin/sw_vers -productVersion)"
-  # restore IFS to previous state
-  IFS=$OLDIFS
-  cmd_line_tools_temp_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-  # Installing the latest Xcode command line tools on 10.9.x or higher
-  if [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -ge 9 ) || ( ${osvers_major} -eq 11 && ${osvers_minor} -ge 0 ) ]]; then
-    # Create the placeholder file which is checked by the softwareupdate tool
-    # before allowing the installation of the Xcode command line tools.
-    touch "$cmd_line_tools_temp_file"
-    # Identify the correct update in the Software Update feed with "Command Line Tools" in the name for the OS version in question.
-    if [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -ge 15 ) || ( ${osvers_major} -eq 11 && ${osvers_minor} -ge 0 ) ]]; then
-       cmd_line_tools=$(softwareupdate -l | awk '/\*\ Label: Command Line Tools/ { $1=$1;print }' | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 9-)
-    elif [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -gt 9 ) ]] && [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -lt 15 ) ]]; then
-       cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "$macos_vers" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
-    elif [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -eq 9 ) ]]; then
-       cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "Mavericks" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
-    fi
-    # Check to see if the softwareupdate tool has returned more than one Xcode
-    # command line tool installation option. If it has, use the last one listed
-    # as that should be the latest Xcode command line tool installer.
-    if (( $(grep -c . <<<"$cmd_line_tools") > 1 )); then
-       cmd_line_tools_output="$cmd_line_tools"
-       cmd_line_tools=$(printf "$cmd_line_tools_output" | tail -1)
-    fi
-    #Install the command line tools
-    softwareupdate -i "$cmd_line_tools" --verbose
-    # Remove the temp file
-    if [[ -f "$cmd_line_tools_temp_file" ]]; then
-      rm "$cmd_line_tools_temp_file"
-    fi
-  fi
+# Installing the latest Xcode command line tools on 10.9.x or higher
+ignoreBeta="true"	# Setting to true will ignore beta. However, setting to false does not guarantee a beta is available.
+# Save current IFS state
+OLDIFS=$IFS
+IFS='.' read osvers_major osvers_minor osvers_dot_version <<< "$(/usr/bin/sw_vers -productVersion)"
+# restore IFS to previous state
+IFS=$OLDIFS
+cmd_line_tools_temp_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+
+if [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -ge 9 ) || ( ${osvers_major} -ge 11 && ${osvers_minor} -ge 0 ) ]]; then
+
+	# Create the placeholder file which is checked by the softwareupdate tool
+	# before allowing the installation of the Xcode command line tools.
+
+	touch "$cmd_line_tools_temp_file"
+
+	# Identify the correct update in the Software Update feed with "Command Line Tools" in the name for the OS version in question.
+
+	if [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -ge 15 ) || ( ${osvers_major} -ge 11 && ${osvers_minor} -ge 0 ) ]]; then
+	   cmd_line_tools=$(softwareupdate -l | awk '/\*\ Label: Command Line Tools/ { $1=$1;print }' | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 9-)
+	elif [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -gt 9 ) ]] && [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -lt 15 ) ]]; then
+	   cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "$osvers_minor" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
+	elif [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -eq 9 ) ]]; then
+	   cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "Mavericks" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
+	fi
+
+	# Check to see if the softwareupdate tool has returned more than one Xcode
+	# command line tool installation option. If it has, use the last one listed
+	# as that should be the latest Xcode command line tool installer.
+
+	if (( $(grep -c . <<<"$cmd_line_tools") > 1 )); then
+	   cmd_line_tools_output="$cmd_line_tools"
+	   cmd_line_tools=$(printf "$cmd_line_tools_output" | tail -1)
+	fi
+	#Install the command line tools
+	softwareupdate -i "$cmd_line_tools" --verbose
+	# Remove the temp file
+	if [[ -f "$cmd_line_tools_temp_file" ]]; then
+	  rm "$cmd_line_tools_temp_file"
+	fi
 fi
+### Script from rtrouton ###
 
 # Test if Homebrew is installed and install it if it is not
 ### INSTALL HOMEBREW ###
